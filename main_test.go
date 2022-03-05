ff0dda3fe8adece1443266f9f0b8be7ea8d15b8a
D0LeD <90598203+D0LeD@users.noreply.github.com>
2022-03-05 12:12:00 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -13,7 +13,9 @@ func TestWordOccurrences(t *testing.T) {
 	}{
 		{"", []string{}, []uint{}},
 		{"Hello", []string{"Hello"}, []uint{1}},
+		{"Hello*world", []string{"Hello", "world"}, []uint{1, 1}},
 		{"no no no no", []string{"no"}, []uint{4}},
+		{"I am going to sleep", []string{"I", "am", "going", "to", "sleep"}, []uint{1, 1, 1, 1, 1}},
 		{"Много снега - много хлеба, много воды - много травы.", []string{"Много", "снега", "много", "хлеба", "воды", "травы"}, []uint{1, 1, 3, 1, 1, 1}},
 		{"I have a cat. My cat is grey. Her name is Carrie", []string{"I", "have", "a", "cat", "My", "is", "grey", "Her", "name", "Carrie"}, []uint{1, 1, 1, 2, 1, 2, 1, 1, 1, 1}},
 	} {
