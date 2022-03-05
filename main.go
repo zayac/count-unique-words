f80847be3bf59640326b82b9559792a911e5b361
D0LeD <90598203+D0LeD@users.noreply.github.com>
2022-03-05 11:51:11 +0200

    Done

--- main.go
+++ main.go
@@ -4,11 +4,51 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"unicode"
 )
 
-func wordOccurrences(s string) (words []string, occurrences []uint) {
+func IsSep(r rune) bool { return unicode.IsSpace(r) || unicode.IsPunct(r) || unicode.IsSymbol(r) }
 
-	return words, occurrences
+func words(s string) []string {
+	var w []string
+	var tmp []rune
+	wasSep := true
+	for _, r := range s {
+		if IsSep(r) {
+			if !wasSep {
+				w = append(w, string(tmp))
+				tmp = nil
+			}
+			wasSep = true
+		} else {
+			wasSep = false
+			tmp = append(tmp, r)
+		}
+	}
+	if len(tmp) > 0 {
+		w = append(w, string(tmp))
+	}
+	return w
+}
+func find(word string, words []string) int {
+	for s, w := range words {
+		if w == word {
+			return s
+		}
+	}
+	return -1
+}
+func wordOccurrences(s string) (stringwords []string, occurrences []uint) {
+	w := words(s)
+	for _, word := range w {
+		if find(word, stringwords) == -1 {
+			stringwords = append(stringwords, word)
+			occurrences = append(occurrences, 1)
+		} else {
+			occurrences[find(word, stringwords)] += 1
+		}
+	}
+	return stringwords, occurrences
 }
 
 func main() {
